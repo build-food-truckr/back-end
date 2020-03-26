5e440cb290e9feb1ef42e8405e988adc3745f702
Joey Nevarez <josephnevarez12@gmail.com>
2020-03-25 17:03:51 -0700

    wrote migration file and register/login routes

--- auth/auth-routes.js
+++ auth/auth-routes.js
@@ -3,9 +3,36 @@ const Users = require('../users/users-model');
 const doesUserAlreadyExist = require("./")
 
 
+function generateToken(user){
+    const payload = {
+        username: user.username,
+        department: user.department
+    }
+    const secret = process.env.JWT_SECRET;
+
+    const options = {
+        expiresIn: '1hr'
+    }
+    return jwt.sign(payload, secret, options)
+};
+
 route.post('/register', async (req, res, next) => {
     try {
+        //check if the client gave all the data
+        const { username, email, password, role } = req.body
+        if (!username || !email || !password || !role){
+            return res.status(400).json({ message: "Please provide full credentials"})
+        }
 
+        //check if the username is already taken
+        const user = await Users.findBy({ username }).first()
+        if(user) {
+            return res.status(400).json({ message: "Username already taken" })
+        }
+
+        //register user in the database, but does not generate token
+        await Users.createUser(req.body)
+        res.status(201).json({ message: `Welcome ${username}!`})
     } catch(err){
         next(err)
     }
@@ -13,7 +40,16 @@ route.post('/register', async (req, res, next) => {
 
 route.post('/login', doesUserAlreadyExist(), async (req, res, next) => {
     try {
+        const { username, password } = req.body
+        const isPasswordValid = bcrypt.compare(password, req.user.password)
 
+        if(!isPasswordValid){
+            res.status(400).json({ message: "Please enter a valid password" })
+        } else {
+            const token = generateToken(req.user)
+            res.cookie("authToken", token)
+            res.json({ message: `Welcome ${username}` })
+        }
     } catch(err){
         next(err)
     }
